0df1fff6d856df549e235cf2616bffc9849543da
Rafly Ramadhani Putra <raflyramadhaniputra28@gmail.com>
2021-12-12 00:20:35 +0700

    setup db atlas

---
+++ index.js
@@ -0,0 +1,39 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+const express = require("express")
+
+const routes = require("./routes")
+const openDBConnection = require("./helpers/db")
+
+
+const port = process.env.PORT || 3000
+const uri = process.env.MONGO_URI || "mongodb+srv://assassincode:assassincode170845@cluster0.abmvz.mongodb.net/db_yia?retryWrites=true&w=majority"
+
+// const dbOptions = {
+//     user: process.env.MONGO_USER,
+//     pass: process.env.MONGO_PASS
+// }
+
+async function main() {
+    try {
+        // mastikan database connect, baru kita jalankan app.
+        await openDBConnection(uri)
+
+        const app = express()
+
+        app.use(express.json()) // biar kita bisa ambil request body.
+
+        app.use(routes)
+
+        app.listen(port, () => {
+            console.log("server is listening on port", port)
+        })
+        
+    } catch (error) {
+        console.log("main:", error)
+    }
+}
+
+main() // menjalankan main.
+
