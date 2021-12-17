12bea68ed3783a2ca02ff97ad8878d9929b4b667
Rafly Ramadhani Putra <raflyramadhaniputra28@gmail.com>
2021-12-17 20:44:42 +0700

    progress awal nyoba AdminaAspirationModels

---
+++ models/AdminAspirationModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const model = new Schema({
+
+  aspiration_title: {
+    type: String,
+    required: true,
+  },
+  aspiration_image: {
+    type: BLOP,
+    required: true,
+  },
+  aspiration_description: {
+    type: String,
+    required: true,
+  },
+
+// timestamp
+
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+  
+}, {
+ 
+ 
+});
+
+module.exports = mongoose.model('todos', model);
