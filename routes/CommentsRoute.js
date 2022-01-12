8f804fe6a11bca2647c54dcba6feb7b5bf4244bf
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2022-01-12 16:37:15 +0700

    update

--- routes/CommentsRoute.js
+++ routes/CommentsRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get("/", CommentsController.getlistAllComment);
 router.post("/", authorize, CommentsController.createComment);
 router.get("/:id", CommentsController.getCommentById);
-router.get("/news/:news_id", authorize, CommentsController.getCommentByNews);
+router.get("/news/:news_id", CommentsController.getCommentByNews);
 router.put("/:id", authorize, CommentsController.updateComment);
 router.delete("/:id", authorize, CommentsController.deleteComment);
 
